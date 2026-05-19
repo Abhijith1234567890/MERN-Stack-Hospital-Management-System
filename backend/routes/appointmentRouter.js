a749f71ab30b3e5b4e29e1a981753938eb09c3bf
abhijith <abhijithpp2001@gmail.com>
2026-05-19 21:40:53 +0530

    Add appointment management functionality with payment confirmation and service appointment creation

---
+++ backend/routes/appointmentRouter.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { clerkMiddleware, requireAuth } from "@clerk/express"
+import { cancelAppointment, confirmPayment, createAppointment, getAppointmentByDoctor, getAppointmentByPatient, getAppointments, getRegisteredUserCount, getStats, updateAppointment } from "../controllers/appointmentController.js"
+
+const appointmentRouter = express.Router()
+
+appointmentRouter.get("/", getAppointments)
+appointmentRouter.get("/confirm", confirmPayment)
+appointmentRouter.get("/status/summary", getStats)
+
+// authentic routes
+appointmentRouter.post("/", clerkMiddleware(), requireAuth(), createAppointment)
+appointmentRouter.get("/me", clerkMiddleware(), requireAuth(), getAppointmentByPatient)
+
+appointmentRouter.get("/doctor/:doctorId", getAppointmentByDoctor)
+
+appointmentRouter.post("/:id/cancel", cancelAppointment)
+appointmentRouter.get("/patients/count", getRegisteredUserCount)
+appointmentRouter.put("/:id", updateAppointment)
+
+export default appointmentRouter
