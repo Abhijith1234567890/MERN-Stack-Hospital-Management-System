f9c5680122b1e3cd9148478793d64d6e65ff0091
abhijith <abhijithpp2001@gmail.com>
2026-05-26 14:50:46 +0530

    feat: add Doctors page with DoctorsPage component and Footer; integrate react-toastify for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import DoctorDetails from "./pages/DoctorDetails";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:id" element={<DoctorDetails />} />
       </Routes>
     </div>
   );
